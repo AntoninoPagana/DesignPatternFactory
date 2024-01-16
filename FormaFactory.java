10dfd3fe281943b502a95766c755b1c05ef0a640
Antonino <pagana899@gmail.com>
2024-01-16 14:20:47 +0100

    Esercizio Completo 2

--- FormaFactory.java
+++ FormaFactory.java
@@ -1,11 +1,11 @@
 public class FormaFactory {
-    public static Forma getForma(EnumForma enumForma) throws Exception{
+    public static Forma getForma(EnumForma enumForma) throws Exception {
         switch (enumForma) {
             case CERCHIO:
                 return new Cerchio();
             case RETTANGOLO:
                 return new Rettangolo();
-            default :
+            default:
                 throw new Exception("Forma non supportata!");
         }
     }
